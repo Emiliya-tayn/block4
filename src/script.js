fa847dfee291f8cafb5a599039d33f765b88452a
Emiliya-tayn <75200265+Emiliya-tayn@users.noreply.github.com>
2020-12-21 12:32:30 +0500

    ver 4

--- src/script.js
+++ src/script.js
@@ -1,15 +1,19 @@
 let burgerOpen = document.querySelector('.header-menu__btn_theme-menu');
 let mobileMenu = document.querySelector('.mobile-menu');
 let burgerClose = mobileMenu.querySelector('.header-mobile-menu__btn');
-
+let hiddenWrapper = document.querySelector('.container');
+let page = document.querySelector('.body');
 
 
 
 burgerOpen.addEventListener('click', function (evt){
     evt.preventDefault();
     mobileMenu.classList.add('mobile-menu_show');
+    hiddenWrapper.classList.add('container_hidden');
+    page.classList.add('body_unscrollable');
 
 })
 burgerClose.addEventListener('click', function (){
     mobileMenu.classList.remove('mobile-menu_show');
+    hiddenWrapper.classList.remove('container_hidden');
 })
