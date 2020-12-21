81d0f86f098ec458c5d85c7fc3923ee8bd714b54
Emiliya-tayn <75200265+Emiliya-tayn@users.noreply.github.com>
2020-12-21 19:04:48 +0500

    swiper

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ let mobileMenu = document.querySelector('.mobile-menu');
 let burgerClose = mobileMenu.querySelector('.header-mobile-menu__btn');
 let hiddenWrapper = document.querySelector('.container');
 let page = document.querySelector('.body');
+let mobileSwiper = document.querySelector('.swiper');
 
 
 
@@ -11,10 +12,12 @@ burgerOpen.addEventListener('click', function (evt){
     mobileMenu.classList.add('mobile-menu_show');
     hiddenWrapper.classList.add('container_hidden');
     page.classList.add('body_unscrollable');
+    mobileSwiper.classList.add('container_hidden');
 
 })
 burgerClose.addEventListener('click', function (){
     mobileMenu.classList.remove('mobile-menu_show');
     hiddenWrapper.classList.remove('container_hidden');
     page.classList.remove('body_unscrollable');
+    mobileSwiper.classList.remove('container_hidden');
 })
---
+++ src/swiper_mobile.js
@@ -0,0 +1,8 @@
+let swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 3,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
