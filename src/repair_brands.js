ce68a2a56eeff63b1f8407a2d7e7cb318aef0cbd
Emiliya-tayn <75200265+Emiliya-tayn@users.noreply.github.com>
2020-12-22 19:30:50 +0500

    swiper+

--- src/repair_brands.js
+++ src/repair_brands.js
@@ -1,26 +1,2 @@
-let buttonMore = document.querySelector('.repair-brands-look-more__btn');
-let show = document.querySelectorAll('.repair-brands-list__item_show');
-let swiperImage = document.querySelector('.repair-brands-look-more__image');
 
-buttonMore.addEventListener('click',function (evt){
-    let hiddenItem = document.querySelectorAll('.swiper__item_hidden');
-    let isNotHaveClass = !hiddenItem[0].classList.contains('swiper__item_show');
 
-    evt.preventDefault();
-
-    if (isNotHaveClass) {
-        for (let i = 0; i < hiddenItem.length; i++) {
-            hiddenItem[i].classList.add('repair-brands-list__item_show');
-        }
-        buttonMore.textContent = 'Скрыть';
-        swiperImage.classList.add('rotate');
-    }
-    else {
-        for (let i = 0; i < hiddenItem.length; i++) {
-            hiddenItem[i].classList.remove('repair-brands-list__item_show');
-        }
-        buttonMore.textContent = 'Показать все';
-        swiperImage.classList.remove('rotate');
-    }
-
-});
--- src/swiper_mobile.js
+++ src/swiper_mobile.js
@@ -1,8 +1,48 @@
-let swiper = new Swiper('.swiper-container', {
-    slidesPerView: 'auto',
-    spaceBetween: 3,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+window.addEventListener("resize", windowSize);
+windowSize();
+function windowSize () {
+    let windowWidth = window.screen.availWidth;
+
+        if(windowWidth < 768) {
+            var swiper = new Swiper('.swiper-container', {
+                slidesPerView: 'auto',
+                spaceBetween: 3,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            })
+        }
+        else {
+            let buttonMore = document.querySelector('.swiper-look-more__btn');
+            let swiperImage = document.querySelector('.swiper-look-more__image');
+            let buttonMoreContainer = document.querySelector('.swiper-look-more');
+
+            buttonMore.addEventListener('click',function (evt){
+                let hiddenItem = document.querySelectorAll('.swiper__slide_hidden');
+                let isNotHaveClass = !hiddenItem[0].classList.contains('swiper__slide_show');
+
+                evt.preventDefault();
+
+                if (isNotHaveClass) {
+                   for (let i = 0; i < hiddenItem.length; i++) {
+                            hiddenItem[i].classList.add('swiper__slide_show');
+                   }
+                   buttonMore.textContent = 'Скрыть';
+                   swiperImage.classList.add('rotate');
+                   buttonMoreContainer.classList.add('swiper-look-more_top');
+                }
+                else {
+                   for (let i = 0; i < hiddenItem.length; i++) {
+                   hiddenItem[i].classList.remove('swiper__slide_show');
+                   }
+                   buttonMore.textContent = 'Показать все';
+                   swiperImage.classList.remove('rotate');
+                   buttonMoreContainer.classList.remove('swiper-look-more_top');
+
+                }
+
+            });
+        }
+
+}
